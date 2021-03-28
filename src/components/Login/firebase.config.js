e2369de801f49c0b7eef9fe010d227e0c77fbef1
md umar faruk <mdumar11233>
2021-03-28 12:51:28 +0600

    start ema john store

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyBws7v1tIeAgZDpE0jPG5uKulVSlYshjmY",
+    authDomain: "ema-john-auth-server.firebaseapp.com",
+    projectId: "ema-john-auth-server",
+    storageBucket: "ema-john-auth-server.appspot.com",
+    messagingSenderId: "8388754669",
+    appId: "1:8388754669:web:bbfc3653337ad537ca60e4"
+  };
+  export default firebaseConfig;
