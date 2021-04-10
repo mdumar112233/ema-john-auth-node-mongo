4e3eaff3d3fedfa970ffc7a21ca781d6b4cc464c
md umar faruk <mdumar11233>
2021-04-10 12:38:13 +0600

    added new code

--- src/components/ProcessPayment/ProcessPayment.js
+++ src/components/ProcessPayment/ProcessPayment.js
@@ -1,29 +1,14 @@
 import React from 'react';
 import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
-import {CardElement} from '@stripe/react-stripe-js';
+import SimpleCardElement from './SimpleCardElement';
 
 const stripePromise = loadStripe('pk_test_51IeAn5F8Tt6idX1YNJvJVrIfNjU3XrD6sHsAdImNegbHw4cdba24Ps7RmrvZ0hrI3uVrTVxzgyhI6SxkDlPcs66700yOJaMdXL');
 
 const ProcessPayment = () => {
     return (
             <Elements stripe={stripePromise}>
-                <CardElement
-                options={{
-                    style: {
-                    base: {
-                        fontSize: '16px',
-                        color: '#424770',
-                        '::placeholder': {
-                        color: '#aab7c4',
-                        },
-                    },
-                    invalid: {
-                        color: '#9e2146',
-                    },
-                    },
-                }}
-                />
+                <SimpleCardElement></SimpleCardElement>
             </Elements>
     );
 };
